99e11bc2132ff1e9ecba8447cc99ecc59e51a752
DavidEscobxr <david.gonzlez3@misena.edu.co>
2023-09-12 13:57:15 -0500

    Primer repositorio Radcook android

---
+++ app/src/main/java/com/example/radcookbotones/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.radcookbotones
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val registerButton = findViewById<Button>(R.id.registerMainButton)
+        val loginButton = findViewById<Button>(R.id.loginMainButton)
+
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/radcookbotones/RegisterActivity.kt
@@ -0,0 +1,17 @@
+package com.example.radcookbotones
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val registerButton = findViewById<Button>(R.id.registerButton)
+        registerButton.setOnClickListener {
+            // Implementa el código para el proceso de registro aquí
+        }
+    }
+}
